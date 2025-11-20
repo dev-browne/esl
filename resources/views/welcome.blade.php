8574548ab6f5b31880a88451adf82ce490c24bb9
dev <dev_browne@hotmail.com>
2025-11-20 09:43:59 +0000

    initial commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" ></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+    @vite(['resources/css/custom.css'])
     <title>Welcome</title>
     </head>
 
@@ -23,10 +24,10 @@
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">My Files</a>
+          <a class="nav-link text-light" href="#">My Files</a>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Reports
           </a>
           <ul class="dropdown-menu">
@@ -36,41 +37,37 @@
           </ul>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">clients</a>
+          <a class="nav-link text-light" href="#">clients</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Adopters</a>
+          <a class="nav-link text-light" href="#">Adopters</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Job Types</a>
+          <a class="nav-link text-light" href="#">Job Types</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Users</a>
+          <a class="nav-link text-light" href="#">Users</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Change Password</a>
+          <a class="nav-link text-light" href="#">Change Password</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Logout</a>
+	<form action="{{ route('logout') }}" method="post">
+         @CSRF
+         <button class="btn text-light"> Logout </button>
+        </form>
         </li>
 
       </ul>
       <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn custom btn-bg" type="submit">Search</button>
+        <a class="btn custom-btn-bg text-light"type="submit">Search</a>
       </form>
     </div>
   </div>
 </nav>
 
-<div class = px-3>
-  
-  <form action="{{ route('logout') }}" method="post">
-    @CSRF
-  <button style="btn text-light"> Logout </button> 
-  </form>
-
 </div>
-    @vite(['resources/css/app.css','resources/js/app.js','resources/css/custom.css'])
+  
 </body>
 </html>
